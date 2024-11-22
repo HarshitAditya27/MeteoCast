c8932b96cfdfd44c4e2243a68bd9e5c032862f53
Harshit Aditya <harshitaditya27@gmail.com>
2024-11-22 23:39:41 +0530

    Added initial setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <Button>Check Weather</Button>
+    </>
+  );
+}
+
+export default App;

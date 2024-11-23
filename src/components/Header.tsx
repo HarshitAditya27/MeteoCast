98a91ac8e1bcc69789845f0ddbda135ad5edd30b
Harshit Aditya <harshitaditya27@gmail.com>
2024-11-23 10:23:00 +0530

    Added header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur py-2 supports-[backdrop-filter]:bg-background/60">
+      <div className="container mx-auto flex h-16 items-center justify-between px-4">
+        <Link to={"/"}>
+          <img src="/logo.png" alt="klogo" className="h-14" />
+        </Link>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
